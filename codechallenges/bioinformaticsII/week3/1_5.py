0c0c2c129ec3b47c0c635a2a7e44d1a1521a4b94
Hans Matthew E. Del Mundo <delmundo.hansmatthew@gmail.com>
2026-05-25 21:24:59 +0800

    approximator of C

--- codechallenges/bioinformaticsII/week3/1_5.py
+++ codechallenges/bioinformaticsII/week3/1_5.py
@@ -7,4 +7,15 @@ p = Path(__file__).parent / "datasets" / "dataset_30216_2.txt"
 
 table = load_integer_mass_table()
 n = int(p.read_text())
-print(BFCountPeptides(n, table))
+nCount = BFCountPeptides(n, table)
+print(nCount)
+
+
+# estimate C
+Cs = []
+for m in range(1200, 1300):
+    Cs.append(BFCountPeptides(m+1, table) / BFCountPeptides(m, table))
+
+C = sum(Cs) / len(Cs)
+
+print(C)
