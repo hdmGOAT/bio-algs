820a7e8a79f99dfff469372a2174c3a21c393f3d
Hans Del Mundo <delmundo.hansmatthew@gmail.com>
2026-07-11 21:43:39 +0800

    memoize

--- codechallenges/bioinformaticsIII/week1/1_5_a.py
+++ codechallenges/bioinformaticsIII/week1/1_5_a.py
@@ -1,12 +1,20 @@
-def RecursiveChange(money, Coins):
+def RecursiveChange(money, Coins, memo=None):
+    if memo is None:
+        memo = {}
+
     if money == 0:
         return 0
+
+    if money in memo:
+        return memo[money]
+
     minNumCoins = float('inf')
     for i in range(len(Coins)):
         if money >=  Coins[i]:
-            numCoins = RecursiveChange(money - Coins[i], Coins)
+            numCoins = RecursiveChange(money - Coins[i], Coins, memo)
             if numCoins + 1 < minNumCoins:
                 minNumCoins = numCoins + 1
+    memo[money] = minNumCoins
     return minNumCoins
 
 print(RecursiveChange(76, [5,4,1]))
