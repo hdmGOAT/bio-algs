d232a1a527b7a562752004040caf06ccf465665a
Hans Matthew E. Del Mundo <delmundo.hansmatthew@gmail.com>
2026-07-15 21:13:22 +0800

    solved a codechallenge

--- codechallenges/bioinformaticsIII/week2/1_2_a.py
+++ codechallenges/bioinformaticsIII/week2/1_2_a.py
@@ -43,3 +43,43 @@ def gaGraph(a, b, match, mismatch, indel):
                 bt[i][j] = "u"
 
     return score, bt
+
+def OutputGA(bt, a, b):
+    i = len(b)
+    j = len(a)
+    aligned_a = []
+    aligned_b = []
+
+    while i > 0 or j > 0:
+        if i > 0 and j > 0 and bt[i][j] == "d":
+            aligned_a.append(a[j-1])
+            aligned_b.append(b[i-1])
+            i -= 1
+            j -= 1
+        elif j > 0 and bt[i][j] == "l":
+            aligned_a.append(a[j-1])
+            aligned_b.append("-")
+            j -= 1
+        elif i > 0 and bt[i][j] == "u":
+            aligned_a.append("-")
+            aligned_b.append(b[i-1])
+            i -= 1
+
+    return "".join(reversed(aligned_a)), "".join(reversed(aligned_b))
+
+from pathlib import Path
+
+p = Path(__file__).parent / "datasets" / "dataset_30199_3.txt"
+
+nums, a, b = p.read_text().splitlines()
+match, mis, indel = map(int,nums.split())
+
+score, bt = gaGraph(a, b, match, mis, indel)
+
+final_score = score[len(b)][len(a)]
+aligned_a, aligned_b = OutputGA(bt, a, b)
+
+with open("output.txt", "w") as f:
+    f.write(f"{final_score}\n")
+    f.write(f"{aligned_a}\n")
+    f.write(f"{aligned_b}\n")
